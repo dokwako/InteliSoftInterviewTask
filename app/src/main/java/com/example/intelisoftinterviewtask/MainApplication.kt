72cd06d6e8260834e6a46a74aa62610c78e7ec79
dokwako <denzilwawire418@gmail.com>
2025-11-05 23:50:34 +0300

    Add InteliSoftInterviewApp Application class

---
+++ app/src/main/java/com/example/intelisoftinterviewtask/MainApplication.kt
@@ -0,0 +1,9 @@
+package com.example.intelisoftinterviewtask
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class InteliSoftInterviewApp : Application() {
+
+}
